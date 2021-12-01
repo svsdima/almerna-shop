cc772d7d012763f4cc6ccb778e2bb50ae4eb545a
Dmitriy <svsdima1995@mail.ru>
2021-12-01 12:54:01 +0300

    product details reducer & action

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,36 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import {listProductDetails} from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const ProductScreen = ({match}) => {
-	const [
-		{
-			_id,
-			name,
-			images,
-			countInStock,
-			price,
-			rating,
-			numReviews,
-			colors,
-			description,
-			height,
-			size,
-			materials,
-		},
-		setProduct,
-	] = useState({});
+	// const [
+	// 	{
+	// 		_id,
+	// 		name,
+	// 		images,
+	// 		countInStock,
+	// 		price,
+	// 		rating,
+	// 		numReviews,
+	// 		colors,
+	// 		description,
+	// 		height,
+	// 		size,
+	// 		materials,
+	// 	},
+	// 	setProduct,
+	// ] = useState({});
+	const dispatch = useDispatch();
+
+	const productDetails = useSelector((state) => state.productDetails);
+	const {loading, error, product} = productDetails;
 
 	useEffect(() => {
-		const fetchProduct = async () => {
-			const {data} = await axios.get(`/api/products/${match.params.id}`);
-
-			setProduct(data);
-		};
-
-		fetchProduct();
-	}, [match]);
+		dispatch(listProductDetails(match.params.id));
+	}, [dispatch]);
 
 	const toggleActiveClass = (e) => {
 		e.target.classList.toggle('active');
@@ -38,66 +39,60 @@ const ProductScreen = ({match}) => {
 	};
 
 	return (
-		<div className={`product-screen ${_id}`}>
+		<div className={`product-screen ${product._id}`}>
 			<div className='container'>
 				<div className='product-screen-nav'>
-					<Link to='/'>Home</Link> / <Link to='/catalog'>Catalog</Link> /
-					<span> Lingerie</span>
+					<Link to='/'>Home</Link> / <Link to='/catalog'>Catalog</Link> /<span> Lingerie</span>
 				</div>
-				<div className='product-screen-wrapper'>
-					<div className='product-screen-images'>
-						<div className='product-screen-images-list'></div>
-						<div className='product-screen-mainImg'>
-							{/* <img src={images} alt={name} /> */}
+				{loading ? (
+					<Loader />
+				) : error ? (
+					<Message>{error}</Message>
+				) : (
+					<div className='product-screen-wrapper'>
+						<div className='product-screen-images'>
+							<div className='product-screen-images-list'></div>
+							<div className='product-screen-mainImg'>
+								<img src={product.images} alt={product.name} />
+							</div>
 						</div>
-					</div>
-					<div className='product-screen-info'>
-						<div className='product-screen-title'>{name}</div>
-						<div className='product-screen-column'>
-							<div className='product-screen-price'>${price}</div>
-							<Rating value={rating} text={`${numReviews} reviews`} />
-						</div>
-						<div className='product-screen-subtitle'>Color</div>
-						<div className='product-screen-colors'></div>
-						<div className='product-screen-subtitle'>
-							Status:{' '}
-							<span>{countInStock > 0 ? 'In Stock' : 'Out of Stock'}</span>
-						</div>
+						<div className='product-screen-info'>
+							<div className='product-screen-title'>{product.name}</div>
+							<div className='product-screen-column'>
+								<div className='product-screen-price'>${product.price}</div>
+								<Rating value={product.rating} text={`${product.numReviews} reviews`} />
+							</div>
+							<div className='product-screen-subtitle'>Color</div>
+							<div className='product-screen-colors'></div>
+							<div className='product-screen-subtitle'>
+								Status: <span>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</span>
+							</div>
 
-						<button className='btn' type='button' disabled={countInStock === 0}>
-							Add to bag by ${price}
-						</button>
-						<div
-							className='product-accordion-title'
-							onClick={toggleActiveClass}
-						>
-							Description
-						</div>
-						<div className='product-accordion-descr'>{description}</div>
-						<div className='product-screen-model'>
-							Model: Height {height} m, Size: {size}
-						</div>
-						<div
-							className='product-accordion-title'
-							onClick={toggleActiveClass}
-						>
-							Materials
-						</div>
-						<div className='product-accordion-descr'>{materials}</div>
-						<div
-							className='product-accordion-title'
-							onClick={toggleActiveClass}
-						>
-							Delivery & Returns
-						</div>
-						<div className='product-accordion-descr'>
-							Вы можете оформить возврат товара в течение 7 дней, не считая дня
-							получения заказа. Правила возврата. Для всех заказов с доставкой в
-							Москву и Ростов-на-Дону, оформленных с 28 октября по 7 ноября,
-							возврат будет для вас бесплатным.
+							<button className='btn' type='button' disabled={product.countInStock === 0}>
+								Add to bag by ${product.price}
+							</button>
+							<div className='product-accordion-title' onClick={toggleActiveClass}>
+								Description
+							</div>
+							<div className='product-accordion-descr'>{product.description}</div>
+							<div className='product-screen-model'>
+								Model: Height {product.height} m, Size: {product.size}
+							</div>
+							<div className='product-accordion-title' onClick={toggleActiveClass}>
+								Materials
+							</div>
+							<div className='product-accordion-descr'>{product.materials}</div>
+							<div className='product-accordion-title' onClick={toggleActiveClass}>
+								Delivery & Returns
+							</div>
+							<div className='product-accordion-descr'>
+								Вы можете оформить возврат товара в течение 7 дней, не считая дня получения заказа.
+								Правила возврата. Для всех заказов с доставкой в Москву и Ростов-на-Дону,
+								оформленных с 28 октября по 7 ноября, возврат будет для вас бесплатным.
+							</div>
 						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		</div>
 	);
