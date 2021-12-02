ccafc28960e9374b4eb2f66c155470e3e709aa40
Dmitriy <svsdima1995@mail.ru>
2021-12-02 11:49:41 +0300

    add to cart functionality

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
 	return (
-		<div class='spinner'>
+		<div className='spinner'>
 			Loading
 			<span></span>
 		</div>
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import Message from '../components/Message';
+import {addToCart} from '../actions/cartActions';
 
-const CartScreen = () => {
+const CartScreen = ({match, location, history}) => {
+	const productId = match.params.id;
+
+	const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+	const dispatch = useDispatch();
+
+	const cart = useSelector((state) => state.cart);
+
+	const {cartItems} = cart;
+
+	console.log(cartItems);
+
+	useEffect(() => {
+		if (productId) {
+			dispatch(addToCart(productId, qty));
+		}
+	}, [dispatch, productId, qty]);
+
+	// console.log(qty);
 	return <div>Cart</div>;
 };
 
