6dfda8eca5cce87f21d4742ac8df928b0b0f7f97
Dmitriy <svsdima1995@mail.ru>
2022-01-24 12:54:56 +0300

    order pay reducer & action

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Message from '../components/Message';
 import { addToCart, removeFromCart } from '../actions/cartActions';
 import { Link } from 'react-router-dom';
 
