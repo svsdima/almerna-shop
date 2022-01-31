590c91b6d3abca969dd5268ab0ce795530980f43
Dmitriy <svsdima1995@mail.ru>
2022-01-31 11:55:08 +0300

    front end product reviews

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,26 +2,53 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Rating from '../components/Rating';
-import { listProductDetails } from '../actions/productActions';
+import { listProductDetails, createReviewProduct } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
 
 const ProductScreen = ({ history, match }) => {
 	const [qty, setQty] = useState(1);
 	const [color, setColor] = useState('');
 	const [bandSize, setBandSize] = useState('');
+	const [rating, setRating] = useState(0);
+	const [comment, setComment] = useState('');
 
 	const dispatch = useDispatch();
 
 	const productDetails = useSelector((state) => state.productDetails);
 	const { loading, error, product } = productDetails;
 
+	const productCreateReview = useSelector((state) => state.productCreateReview);
+	const { error: errorProductReview, success: successProductReview } = productCreateReview;
+
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
 	useEffect(() => {
+		if (successProductReview) {
+			alert('Review Submitted!');
+			setRating(0);
+			setComment('');
+			dispatch({ type: PRODUCT_CREATE_REVIEW_RESET });
+		}
+
 		dispatch(listProductDetails(match.params.id));
-	}, [dispatch, match]);
+	}, [dispatch, match, successProductReview]);
 
 	const addToCartHandler = () => {
 		history.push(`/cart/${match.params.id}?qty=${qty}?size=${bandSize}?color=${color}`);
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		dispatch(
+			createReviewProduct(match.params.id, {
+				rating,
+				comment,
+			})
+		);
 	};
 
 	const toggleActiveClass = (e) => {
@@ -172,6 +199,53 @@ const ProductScreen = ({ history, match }) => {
 										заказа. Правила возврата. Для всех заказов с доставкой в Москву и
 										Ростов-на-Дону, оформленных с 28 октября по 7 ноября, возврат будет для вас
 										бесплатным.
+									</div>
+								</div>
+								<div className='product-screen-comments'>
+									<h2 className='subtitle'>Reviews</h2>
+									{product.reviews.length === 0 && <Message>No Reviews</Message>}
+									<ul>
+										{product.reviews.map((review) => (
+											<li key={review._id}>
+												<strong>{review.name}</strong>
+												<Rating value={review.rating} />
+												<p>{review.createdAt.substring(0, 10)}</p>
+												<p>{review.comment}</p>
+											</li>
+										))}
+									</ul>
+									<div>
+										<h2 className='subtitle'>Write a Customer Review</h2>
+										{errorProductReview && <Message>{errorProductReview}</Message>}
+										{userInfo ? (
+											<form onSubmit={submitHandler}>
+												<div id='rating'>
+													<label>Rating</label>
+													<select value={rating} onChange={(e) => setRating(e.target.value)}>
+														<option value=''>Select...</option>
+														<option value='1'>1 - Poor</option>
+														<option value='2'>2 - Fair</option>
+														<option value='3'>3 - Good</option>
+														<option value='4'>4 - Vary Good</option>
+														<option value='5'>5 - Excellent</option>
+													</select>
+												</div>
+												<div id='comment'>
+													<label>Comment</label>
+													<textarea
+														value={comment}
+														onChange={(e) => setComment(e.target.value)}
+													></textarea>
+												</div>
+												<button className='btn' type='submit'>
+													Submit
+												</button>
+											</form>
+										) : (
+											<Message>
+												Please <Link to='/login'>Sign In</Link> to write a review
+											</Message>
+										)}
 									</div>
 								</div>
 							</div>
